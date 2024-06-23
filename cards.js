4bd48313a65c93f21ce211c47c152f1c5c396875
hunglb <hunglb@techlab.ai>
2024-06-24 01:48:39 +0700

    dev

--- cards.js
+++ cards.js
@@ -6,6 +6,71 @@ var data = {
         '氵户': '沪',
         '火户': '炉'
     },
+    correctMap : {
+        '女' : {
+            '女子': '好 hǎo',
+            '女也': '她 tā',
+            '女马': '妈 mā',
+            '女未': '妹 mèi',
+            '女且': '姐 jiě',
+            '女生': '姓 xìng'
+        },
+        '亻' : {
+            '亻尔':'姓 xìng',
+            '亻也':'他 tā',
+            '亻我':'俄 é',
+            '亻言':'信 xīn ',
+            '亻本':'体 tǐ',
+            '亻生':'体 tǐ',
+            '亻立':'位 wèi',
+            '亻两':'俩 liǎ',
+            '亻故':'做 zuò',
+            '亻旦':'但 dān',
+            '亻牛':'件 jiàn'
+        },
+        '口' : {
+            '口马':'吗 ma',
+            '口曷':'喝 hē',
+            '口丩':'叫 jiào',
+            '口乞':'吃 chī',
+            '口卑':'啤 pí',
+            '口斤':'听 tīng'
+        },
+        '氵' : {
+            '氵又':'汉 hàn',
+            '氵去':'法 fǎ',
+            '氵殳':'没 méi',
+            '氵气':'汽 qì'
+        },
+        ' 讠' : {
+            ' 讠巷':'港 xiāng',
+            ' 讠酉':'酒 jiǔ',
+            ' 讠青':'请 qǐng',
+            ' 讠射':'谢 xiè',
+            ' 讠舌':'话 huà'
+        },
+        '又' : {
+            '又隹':'难 nán',
+            '又寸':'对 duì',
+            '又鸟':'鸡 jī',
+            '又欠':'欢 huān'
+        },
+        '饣' : {
+            '饣反':'欢 huān',
+            '饣官':'馆 guǎn',
+            '饣曼':'馒 mán',
+            '饣交':'饺 jiǎo'
+        },
+        '扌' : {
+            '扌戈':'找 zhǎo',
+            '扌奂':'换 huàn',
+            '扌受':'授 shòu'
+        },
+        '日' : {
+            '日月':'明 míng',
+            '日乍':'昨 zuó'
+        }
+    },
     selectedComponents: [],
     result: ''
 }
@@ -119,26 +184,35 @@ function shuffleCards() {
     }
 }
 
-function startGame() {
-    //arrange the board 4x5
-    for (let r = 0; r < 4; r++) {
-        let row = [];
-        for (let c = 0; c < 3; c++) {
-            let character = charList.pop();
-            row.push(character); //JS
+// function startGame() {
+//     //arrange the board 4x5
+//     for (let r = 0; r < 4; r++) {
+//         let row = [];
+//         for (let c = 0; c < 3; c++) {
+//             let character = charList.pop();
+//             row.push(character); //JS
+//
+//             let box = document.createElement("button");
+//             box.setAttribute("id", "button2");
+//             box.setAttribute("class", "button");
+//             box.textContent = character;
+//             box.setAttribute("disabled", true)
+//            
+//             box.addEventListener("click", selectComponent);
+//             document.getElementById("board").append(box);
+//
+//         }
+//         board.push(row);
+//     }
+// }
 
-            let box = document.createElement("button");
-            box.setAttribute("id", "button2");
-            box.setAttribute("class", "button");
-            box.textContent = character;
-            box.setAttribute("disabled", true)
-            
-            box.addEventListener("click", selectComponent);
-            document.getElementById("board").append(box);
+function startGame2() {
 
-        }
-        board.push(row);
+    for (let center in data.correctMap) {
+        
     }
+    
+    
 }
 
 function disableButtonsInDiv(divId) {
