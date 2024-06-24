1c0c4cf0d98f2270f01792ed7a55842a6ab28c04
hunglb <hunglb@techlab.ai>
2024-06-25 01:47:42 +0700

    dev

--- cards.js
+++ cards.js
@@ -1,82 +1,74 @@
 var data = {
-    components: ['子', '女', '亻', '户', '氵', '火'],
-    correctCombinations: {
-        '女子': '好',
-        '亻子': '仔',
-        '氵户': '沪',
-        '火户': '炉'
+    pairFlow : [['女'], ['亻'], ['口','又'], ['氵',' 讠'], ['饣','扌','日']],
+    incorrectMap: {
+        '女' : ['日',' 甘','曷','末'],
+        '亻' : ['日',' 那','找']
     },
-    pairFlow : [['女'], ['亻']],
-    correctMap : {
-        '女' : {
-            '女子': '好 hǎo',
-            '女也': '她 tā',
-            '女马': '妈 mā',
-            '女未': '妹 mèi',
-            '女且': '姐 jiě',
-            '女生': '姓 xìng'
-        },
-        '亻' : {
-            '亻尔':'姓 xìng',
-            '亻也':'他 tā',
-            '亻我':'俄 é',
-            '亻言':'信 xīn ',
-            '亻本':'体 tǐ',
-            '亻生':'体 tǐ',
-            '亻立':'位 wèi',
-            '亻两':'俩 liǎ',
-            '亻故':'做 zuò',
-            '亻旦':'但 dān',
-            '亻牛':'件 jiàn'
-        },
-        '口' : {
-            '口马':'吗 ma',
-            '口曷':'喝 hē',
-            '口丩':'叫 jiào',
-            '口乞':'吃 chī',
-            '口卑':'啤 pí',
-            '口斤':'听 tīng'
-        },
-        '氵' : {
-            '氵又':'汉 hàn',
-            '氵去':'法 fǎ',
-            '氵殳':'没 méi',
-            '氵气':'汽 qì'
-        },
-        ' 讠' : {
-            ' 讠巷':'港 xiāng',
-            ' 讠酉':'酒 jiǔ',
-            ' 讠青':'请 qǐng',
-            ' 讠射':'谢 xiè',
-            ' 讠舌':'话 huà'
-        },
-        '又' : {
-            '又隹':'难 nán',
-            '又寸':'对 duì',
-            '又鸟':'鸡 jī',
-            '又欠':'欢 huān'
-        },
-        '饣' : {
-            '饣反':'欢 huān',
-            '饣官':'馆 guǎn',
-            '饣曼':'馒 mán',
-            '饣交':'饺 jiǎo'
-        },
-        '扌' : {
-            '扌戈':'找 zhǎo',
-            '扌奂':'换 huàn',
-            '扌受':'授 shòu'
-        },
-        '日' : {
-            '日月':'明 míng',
-            '日乍':'昨 zuó'
-        }
+    correctMap: {
+        '女' : ['子','也','马','未','且','生'],
+        '亻' : ['尔','也','我','言','本', '生','立','两','故','旦','牛'],
+        '口' : ['马','曷','丩','乞','卑', '斤','那','隹','寸','鸟','欠'],
+        '氵' : ['又','去','殳','气','巷', '酉','青','舌'],
+        '讠' : ['青','射','舌','殳'],
+        '又' : ['隹','寸','鸟','欠'],
+        '饣' : ['反','官','曼','交'],
+        '扌' : ['戈','奂','受'],
+        '日' : ['月','乍']
+    },
+    correctCombinations : {
+        '女子': '好 hǎo',
+        '女也': '她 tā',
+        '女马': '妈 mā',
+        '女未': '妹 mèi',
+        '女且': '姐 jiě',
+        '女生': '姓 xìng',
+        
+        '亻尔': '你 nǐ',
+        '亻也': '他 tā',
+        '亻我': '俄 é',
+        '亻言': '信 xīn ',
+        '亻本': '体 tǐ',
+        '亻生': '住 zhù',
+        '亻立': '位 wèi',
+        '亻两': '俩 liǎ',
+        '亻故': '做 zuò',
+        '亻旦': '但 dān',
+        '亻牛': '件 jiàn',
+        
+        '口马': '吗 ma',
+        '口曷': '喝 hē',
+        '口丩': '叫 jiào',
+        '口乞': '吃 chī',
+        '口卑': '啤 pí',
+        '口斤': '听 tīng',
+        '氵又': '汉 hàn',
+        '氵去': '法 fǎ',
+        '氵殳': '没 méi',
+        '氵气': '汽 qì',
+        ' 讠巷': '港 xiāng',
+        ' 讠酉': '酒 jiǔ',
+        ' 讠青': '请 qǐng',
+        ' 讠射': '谢 xiè',
+        ' 讠舌': '话 huà',
+        '又隹': '难 nán',
+        '又寸': '对 duì',
+        '又鸟': '鸡 jī',
+        '又欠': '欢 huān',
+        '饣反': '欢 huān',
+        '饣官': '馆 guǎn',
+        '饣曼': '馒 mán',
+        '饣交': '饺 jiǎo',
+        '扌戈': '找 zhǎo',
+        '扌奂': '换 huàn',
+        '扌受': '授 shòu',
+        '日月': '明 míng',
+        '日乍': '昨 zuó'
     },
     selectedComponents: [],
     result: ''
 }
 let timerInterval;
-let remainingTime = 60000; // 5 minutes in milliseconds
+let remainingTime = 180000; // 5 minutes in milliseconds
 
 let correctPair = [];
 
@@ -86,8 +78,13 @@ function updateTime() {
     } else {
         clearInterval(timerInterval); // Stop the timer when it reaches 0
         disableButtonsInDiv('board')
-        document.getElementById('message').textContent = 'Time is up!';
-        document.getElementById("score2").innerHTML  = "Score: " + correctPair.length;
+        if (correctPair.length === Object.keys(data.correctCombinations).length) {
+            document.getElementById('message').textContent = 'You winnn!';
+        } else {
+            document.getElementById('message').textContent = 'Time is up!';
+        }
+        document.getElementById("score2").innerHTML  = "Correct: " + correctPair.join(" ,");
+
         remainingTime = 0;
     }
 
@@ -117,12 +114,13 @@ function stopTimer() {
 
 function resetTimer() {
     stopTimer();
-    remainingTime = 60000; // Reset to 5 minutes
-    document.getElementById('timer').textContent = '01:00';
+    remainingTime = 180000; // Reset to 5 minutes
+    document.getElementById('timer').textContent = '03:00';
     document.getElementById('message').textContent = ''; // Clear any previous message
     disableButtonsInDiv('board');
 }
 
+let timer;
 
 function selectComponent(event) { 
     const selected = event.target.innerHTML;
@@ -135,32 +133,48 @@ function selectComponent(event) {
     }
 
     if (selectedComponents.length === 2) {
+        clearTimeout(timer)
         const combined = selectedComponents.join('');
         let result = correctCombinations[combined];
         if (result) {
             if (!correctPair.includes(result)) {
                 correctPair.push(result);
             }
-            document.getElementById("score").innerHTML  = "Correct: " + correctPair.join(" ,");
+            document.getElementById("score").innerHTML  = "Score: " + correctPair.length;
+            checkNextLevel();
         } else {
            result = '错误组合'; 
         }
         
         data.result = result;
         document.getElementById("result").innerHTML  = result;
-        
-        disableButtonsInDiv("board");
-        resetSelection();
+        data.selectedComponents = [];
+        data.result = '';
+        timer = resetSelection();
     }
 }
 
 function resetSelection() {
-    setTimeout(() => {
-        data.selectedComponents = [];
-        data.result = '';
-        document.getElementById("result").innerHTML  = data.result;
+    return setTimeout(() => {
+        document.getElementById("result").innerHTML  = '';
         enableButtonsInDiv('board')
-    }, 500);
+    }, 4000);
+}
+
+let currentLevel = 0;
+
+function checkNextLevel() {
+    let maxScore = 0;
+    for (let i = 0; i <= currentLevel; i++) {
+        data.pairFlow[i].forEach(item => maxScore += data.correctMap[item].length);
+    }
+    if (correctPair.length === maxScore) {
+        if (currentLevel + 1 !== data.pairFlow.length) {
+            document.getElementById('center').remove();
+            document.getElementById('board').querySelectorAll('button').forEach(i => i.remove())
+        }
+        nextLevel(++currentLevel)
+    }
 }
 
 
@@ -169,27 +183,22 @@ var board = [];
 
 window.onload = function () {
     resetTimer();
-    shuffleCards2(0);
-    startGame2(0);
+    nextLevel(0)
 }
 
-function shuffleCards() {
-    charList = Array(2).fill(data.components).reduce((a, b) => a.concat(b));
-    //shuffle
-    for (let i = 0; i < charList.length; i++) {
-        let j = Math.floor(Math.random() * charList.length); //get random index
-        //swap
-        let temp = charList[i];
-        charList[i] = charList[j];
-        charList[j] = temp;
-    }
+function nextLevel(centerIndex) {
+    shuffleCards(centerIndex);
+    startGame(centerIndex);
 }
 
-function shuffleCards2(centerIndex) {
+function shuffleCards(centerIndex) {
+    if (centerIndex === data.pairFlow.length) return;
     for (let i = 0; i < data.pairFlow[centerIndex].length ; i++) {
-        for (const outerKey in data.correctMap[data.pairFlow[centerIndex][i]]) {
-            const innerKeys = outerKey.replace(data.pairFlow[centerIndex][i], "");
-            charList.push(innerKeys)
+        if (data.correctMap.hasOwnProperty(data.pairFlow[centerIndex][i])) {
+            charList.push(...data.correctMap[data.pairFlow[centerIndex][i]]);
+        }
+        if (data.incorrectMap.hasOwnProperty(data.pairFlow[centerIndex][i])) {
+            charList.push(...data.incorrectMap[data.pairFlow[centerIndex][i]]);
         }
     }
     //shuffle
@@ -200,32 +209,13 @@ function shuffleCards2(centerIndex) {
         charList[i] = charList[j];
         charList[j] = temp;
     }
-    console.log("charList " + charList);
 }
 
-// function startGame() {
-//     //arrange the board 4x5
-//     for (let r = 0; r < 4; r++) {
-//         let row = [];
-//         for (let c = 0; c < 3; c++) {
-//             let character = charList.pop();
-//             row.push(character); //JS
-//
-//             let box = document.createElement("button");
-//             box.setAttribute("id", "button2");
-//             box.setAttribute("class", "button");
-//             box.textContent = character;
-//             box.setAttribute("disabled", true)
-//            
-//             box.addEventListener("click", selectComponent);
-//             document.getElementById("board").append(box);
-//
-//         }
-//         board.push(row);
-//     }
-// }
+function startGame(centerIndex) {
+    if (currentLevel + 1 <= data.pairFlow.length) {
+        document.getElementById("level").innerHTML = "Level: " + (currentLevel + 1) + "/" + data.pairFlow.length;
+    }
 
-function startGame2(centerIndex) {
 
     if (centerIndex < data.pairFlow.length) {
         for (let i = 0; i < data.pairFlow[centerIndex].length; i++) {
@@ -233,58 +223,83 @@ function startGame2(centerIndex) {
             center.setAttribute("id", "center");
             center.setAttribute("class", "side-buttons, clickable");
             center.textContent = data.pairFlow[centerIndex][i];
-            center.setAttribute("disabled", true)
+            if (centerIndex === 0) {
+                center.setAttribute("disabled", true)
+            }
 
             center.addEventListener("click", selectComponent);
             document.getElementById("board").append(center);
+            addListenerChangeColor(center)
         }
-
-        for (let j = 0; j < Math.floor(charList.length / 4) ; j++) {
+        let charLength = charList.length;
+        for (let j = 0; j < Math.floor(charLength / 4) ; j++) {
             let top = document.createElement("button");
             top.setAttribute("class", "clickable");
             top.textContent = charList.pop();
-            top.setAttribute("disabled", true)
+            if (centerIndex === 0) {
+                top.setAttribute("disabled", true)
+            }
 
             top.addEventListener("click", selectComponent);
             document.getElementById("topId").append(top);
-        }
+            addListenerChangeColor(top)
 
-        for (let j = 0; j < Math.floor(charList.length / 4); j++) {
             let left = document.createElement("button");
             left.setAttribute("class", "clickable");
             left.textContent = charList.pop();
-            left.setAttribute("disabled", true)
+            if (centerIndex === 0) {
+                left.setAttribute("disabled", true)
+            }
 
             left.addEventListener("click", selectComponent);
             document.getElementById("leftId").append(left);
-        }
+            addListenerChangeColor(left)
 
-        for (let j = 0; j < Math.floor(charList.length / 4); j++) {
             let right = document.createElement("button");
             right.setAttribute("class", "clickable");
             right.textContent = charList.pop();
-            right.setAttribute("disabled", true)
+            if (centerIndex === 0) {
+                right.setAttribute("disabled", true)
+            }
 
             right.addEventListener("click", selectComponent);
             document.getElementById("rightId").append(right);
+            addListenerChangeColor(right)
         }
+        charLength = charList.length;
 
-        for (let i = 0; i < charList.length; i++) {
+        for (let i = 0; i < charLength; i++) {
             let bottom = document.createElement("button");
             bottom.setAttribute("class", "clickable");
             bottom.textContent = charList.pop();
-            bottom.setAttribute("disabled", true)
+            if (centerIndex === 0) {
+                bottom.setAttribute("disabled", true)
+            }
 
             bottom.addEventListener("click", selectComponent);
             document.getElementById("bottomId").append(bottom);
+            addListenerChangeColor(bottom)
 
         }
 
     } else {
+        console.log("out")
         remainingTime = 0;
     }
-    
-    
+}
+
+let lastClickedButton = null;
+
+function addListenerChangeColor(item) {
+    item.addEventListener('click', function () {
+        if (lastClickedButton) {
+            lastClickedButton.classList.remove('clicked');
+            lastClickedButton = null;
+        } else {
+            this.classList.add('clicked');
+            lastClickedButton = this;
+        }
+    });
 }
 
 function disableButtonsInDiv(divId) {
